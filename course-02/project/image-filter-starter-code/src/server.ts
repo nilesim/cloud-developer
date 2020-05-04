6f482268c8f2dcdd5d61d29f45ba2979a15dceb7
Selin <selin@Selins-MacBook-Air.local>
2020-05-04 17:29:06 +0300

    validations for image and url

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -6,7 +6,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Init the Express application
   const app = express();
-
+  const validator = require('validator');
   // Set the network port
   const port = process.env.PORT || 8082;
   
@@ -16,17 +16,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
+  app.get( "/filteredimage",
+    async ( req, res ) => {
+    let { image_url } = req.query;
+
+      if ( !image_url ) {
+        return res.status(400)
+                  .send(`image_url is required`);
+      }
+    // IT SHOULD
+    //    1. validate the image_url query
+    if (!validator.isURL(image_url)) {
+      return res.status(400)
+                  .send(`url is invalid`);
+    }
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
+  const filePath = await filterImageFromURL(image_url);
+    res.sendFile(filePath, function(){
+      deleteLocalFiles([filePath]);
+    });
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+  } );
+  
   /**************************************************************************** */
 
   //! END @TODO1
