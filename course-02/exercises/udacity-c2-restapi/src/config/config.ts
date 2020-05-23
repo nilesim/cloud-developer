e1f74d09b8ed195553a75fd4bc94e919ab95e2c7
Selin <selin@Selins-MacBook-Air.local>
2020-05-23 20:38:58 +0300

    login and post successfully made

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagramselindev",
-    "password": "udagramselindev",
-    "database": "udagramselindev",
-    "host": "udagramselindev.cbokukz9elyj.us-east-2.rds.amazonaws.com",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
@@ -17,6 +17,6 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "secret": "helloworld"
+    "secret": process.env.JWT_SECRET
   }
 }
