fb3911eb4c448ed52f800334c5f918e423f157de
Selin <selin@Selins-MacBook-Air.local>
2020-05-24 10:33:03 +0300

    feed and user microservice

---
+++ course-02/exercises/udacity-c2-restapi-feed/try.sh
@@ -0,0 +1,4 @@
+export Postgress_Username=udagramselindev
+export Postgress_Password=udagramselindev
+export Postgress_Database=udagramselindev
+export Postgress_Host=udagramselindev.cbokukz9elyj.us-east-2.rds.amazonaws.com
